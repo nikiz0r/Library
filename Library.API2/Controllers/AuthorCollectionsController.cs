666e31a18b9453c28ace4384df641b71e5ecedc8
Rumenigue Nogueira da Silva <niguerag@gmail.com>
2018-08-28 18:18:30 -0300

    Working with array and composite keys

--- Library.API2/Controllers/AuthorCollectionsController.cs
+++ Library.API2/Controllers/AuthorCollectionsController.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using AutoMapper;
 using Library.API2.Entities;
+using Library.API2.Helpers;
 using Library.API2.Models;
 using Library.API2.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -31,7 +33,24 @@ namespace Library.API2.Controllers
 
             if (!_libraryRepository.Save()) throw new Exception("Creating author collection failed on save.");
 
-            return new OkResult();
+            var authorCollectionToReturn = Mapper.Map<IEnumerable<AuthorDto>>(authorEntities);
+            var idsAsString = string.Join(",", authorCollectionToReturn.Select(a => a.Id));
+
+            return CreatedAtRoute("AuthorCollection", new { ids = idsAsString }, authorCollectionToReturn);
+        }
+
+        [HttpGet("({ids})", Name = "GetAuthorCollection")]
+        public IActionResult AuthorCollection(
+            [ModelBinder(BinderType = typeof(ArrayModelBinder))] IEnumerable<Guid> ids)
+        {
+            if (ids == null) return BadRequest();
+
+            var authorEntities = _libraryRepository.GetAuthors(ids);
+
+            if (ids.Count() != authorEntities.Count()) return NotFound();
+
+            var authorsToReturn = Mapper.Map<IEnumerable<AuthorDto>>(authorEntities);
+            return Ok(authorsToReturn);
         }
     }
 }
